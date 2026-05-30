230876fe7430dd3f88408e969dd94f9f60efcc98
jackmacha914-hash <jackmacha914@gmail.com>
2026-05-30 22:20:05 +0300

    User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
   role: { 
     type: String, 
     required: true, 
-    enum: ['student', 'teacher', 'admin'],
+    enum: ['student', 'teacher', 'admin', 'accountant', 'superadmin'],
     default: 'student'
   },
   class: { 
