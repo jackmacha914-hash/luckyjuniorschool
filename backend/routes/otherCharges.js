585a42341c92b368d48850e09cc1a9cebb1114c9
jackmacha914-hash <jackmacha914@gmail.com>
2026-05-30 14:29:22 +0300

    otherCharges.js

--- backend/routes/otherCharges.js
+++ backend/routes/otherCharges.js
@@ -4,16 +4,42 @@ const router = express.Router();
 const OtherCharge = require('../models/OtherCharge');
 
 
-// GET all charges
+// GET all charges (FILTERED)
 router.get('/', async (req, res) => {
     try {
 
-        const { className } = req.query;
+        const { className, chargeType, date, search } = req.query;
 
         let filter = {};
 
+        // Class filter
         if (className) {
             filter.className = className;
+        }
+
+        // Charge type filter (Diary, Book, Trip, etc.)
+        if (chargeType) {
+            filter.chargeType = chargeType;
+        }
+
+        // Date filter (safe same-day range)
+        if (date) {
+            const start = new Date(date);
+            const end = new Date(date);
+            end.setDate(end.getDate() + 1);
+
+            filter.date = {
+                $gte: start,
+                $lt: end
+            };
+        }
+
+        // Student search filter
+        if (search) {
+            filter.studentName = {
+                $regex: search,
+                $options: "i"
+            };
         }
 
         const charges = await OtherCharge.find(filter)
@@ -22,13 +48,12 @@ router.get('/', async (req, res) => {
         res.json(charges);
 
     } catch (err) {
-        console.error(err);
+        console.error("Other Charges GET error:", err);
         res.status(500).json({
             message: 'Server Error'
         });
     }
 });
-
 
 // CREATE charge
 router.post('/', async (req, res) => {
