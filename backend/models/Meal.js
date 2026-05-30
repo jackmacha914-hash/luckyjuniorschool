74ef1b23f80b22ff63dfc4adb5383dec0bac9553
jackmacha914-hash <jackmacha914@gmail.com>
2026-05-30 12:07:03 +0300

    Meal.js

--- backend/models/Meal.js
+++ backend/models/Meal.js
@@ -1,13 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const mealSchema = new mongoose.Schema({
-    className: String,
-    studentName: String,
-    mealType: String,
-    date: Date,
-    frequency: String,
-    amount: Number,
-    receiptNumber: String
+    className: {
+        type: String,
+        required: true
+    },
+    studentName: {
+        type: String,
+        required: true
+    },
+    mealType: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    frequency: {
+        type: String
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    receiptNumber: {
+        type: String,
+        required: true
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Meal", mealSchema);
+module.exports = mongoose.model('Meal', mealSchema);
