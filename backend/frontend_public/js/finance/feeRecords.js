f64a195604157c13c33282dc01919513f8d95425
jackmacha914-hash <jackmacha914@gmail.com>
2026-05-31 06:12:55 +0300

    feeRecords.js

---
+++ backend/frontend_public/js/finance/feeRecords.js
@@ -0,0 +1,11 @@
+// Load Fee Records
+async function loadFeeRecords() {
+    console.log('Loading fee records...');
+
+    // KEEP YOUR FULL loadFeeRecords FUNCTION HERE EXACTLY AS IT IS
+}
+
+// View Fee Details
+function viewFeeDetails(feeId) {
+    console.log('Viewing fee details for:', feeId);
+}
